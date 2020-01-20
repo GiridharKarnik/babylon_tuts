059df37691a173d062e51394de36e0e3cf840f6f
Giridhar Mohan <moha9001@gb-loc-al-u4185.imgworld.org>
2020-01-20 16:11:47 +0000

    Creating and controlling a spot light source

--- index.js
+++ index.js
@@ -3,13 +3,14 @@ window.addEventListener("DOMContentLoaded", function() {
   let engine = new BABYLON.Engine(canvas, true);
 
   let createScene = function() {
+    //step 1: crate a scene.
     let scene = new BABYLON.Scene(engine);
     scene.clearColor = new BABYLON.Color3.White();
 
-
+    //step 2: crate a 3D object in the scene
     let box = BABYLON.Mesh.CreateBox("Box", 4.0, scene);
-    
-    //viewport, an eye.
+
+    //step 3: crate a camera to view the world from. viewport, an eye.
     let camera = new BABYLON.ArcRotateCamera(
       "arcCamera",
       BABYLON.Tools.ToRadians(45),
@@ -20,6 +21,7 @@ window.addEventListener("DOMContentLoaded", function() {
     );
 
     /*
+        step 4: camera controls.
         attaching the the canvas controls to the camera 
         now we can provide key board inputs to the camera.
     */
@@ -31,15 +33,45 @@ window.addEventListener("DOMContentLoaded", function() {
     camera.keysLeft.push(65);
     camera.keysRight.push(68);
 
+    //step 5: create a light source
+    let light = new BABYLON.PointLight(
+      "pointLight",
+      new BABYLON.Vector3(0, 10, 5),
+      scene
+    );
+
+    //color of the light
+    light.diffuse = new BABYLON.Color3(1, 0, 0);
+
+    //controlling the lights
+    scene.actionManager = new BABYLON.ActionManager(scene);
+    scene.actionManager.registerAction(
+      new BABYLON.ExecuteCodeAction(
+        {
+          trigger: BABYLON.ActionManager.OnKeyUpTrigger,
+          parameter: " "
+        },
+        () => {
+          light.setEnabled(!light.isEnabled());
+        }
+      )
+    );
 
     return scene;
   };
 
   let scene = createScene();
 
+  //final step: start the game loop.
   //will be called 60 times per second, because babylon.js is capped at 60fps.
   //this method is called to draw the world.
   engine.runRenderLoop(function() {
+
+    //change the light color every render
+    let light = scene.getLightByName("pointLight");
+    light.diffuse.g += 0.01;
+    light.diffuse.b += 0.01;
+    
     scene.render();
   });
 });
